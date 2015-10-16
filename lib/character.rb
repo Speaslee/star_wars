199455c8b43c184b4c9a41ea868f3d61341f3358
Speaslee <sophiapeaslee@gmail.com>
2015-10-16 01:14:16 -0400

    terrible code

--- lib/character.rb
+++ lib/character.rb
@@ -1,7 +1,5 @@
 require 'mechanize'
 
-require './lib/traversal.rb'
-
 class Character < ActiveRecord::Base
 validates_presence_of :name
 validates_uniqueness_of :name
@@ -15,7 +13,69 @@ has_many :affiliations, through: :memberships
     link = page1.link_with(text: 'characters')
     page2 = link.click
     link = page2.link_with(text: 'Individuals by faction')
-    page = link.click.go!
+    page = link.click
+
+      visited_links = []
+      good_links = []
+
+      page.links.each do |l|
+        if l.href== nil || l.href.include?("?") || l.href.include?("Policies") || l.text == "Articles" || l.text == "Status Articles" || l.text == "Browse articles by category"  || l.text == "Contact" || l.text == "Forums" || l.href.include?("talk")
+          next
+        elsif l.href.include?("Category") == false
+          next
+        end
+        good_links.push l
+      end
+
+      potential_character_links = []
+      sub_categories = []
+
+      loop do
+      if good_links.any?
+      good_links.each do |l|
+        l = good_links.first.click
+        good_links.shift
+        sub_categories.push l
+          end
+      else
+        break
+      end
+      sub_categories
+      end
+
+loop do
+  sub_sub_categories = []
+
+  if sub_categories.any?
+
+    sub_categories.links.each do |l|
+      if l.href== nil || l.href.include?("?") || l.href.include?("Policies") || l.text == "Articles" || l.text == "Status Articles" || l.text == "Browse articles by category"  || l.text == "Contact" || l.text == "Forums" || l.href.include?("talk")
+        next
+      elsif l.href.include?("Category")== false && l.href.include?("Special")== false && l.href.include?("Wookieepedia")== false
+        potential_character_links.push l
+      else
+      sub_sub_categories.push l
+    end
+  end
+
+    binding.pry
+    sub_categories.links.each
+      t = sub_categories.first.click
+      sub_categories.shift
+      sub_categories.push t
+    end
+  end
+    binding.pry
+  else
+    sub_sub_categories = sub_categories
+  when sub_sub_categories.any? == false && sub_categories.any? == false
+    break
+  end
+  puts potential_character_links
+end
+
+
+  binding.pry
 
 
 
@@ -28,9 +88,10 @@ has_many :affiliations, through: :memberships
     if cells.count == 2
       key = cells.first.downcase.text
       value = cells.last.text
-      results[key] = value
 
+      results[key] = value
       end
+
 
     results[affiliations].value
     end
