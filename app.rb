346b71db3473d340298336ee4ef6b7993fd5b6fa
Speaslee <sophiapeaslee@gmail.com>
2015-10-19 11:56:54 -0400

    troubleshooting app, can't search by name but can by id

--- app.rb
+++ app.rb
@@ -7,73 +7,55 @@ class App < Sinatra::Base
 
 
   get "/character/:name" do
-  #   s_name = params[:name]
-  # s = Character.where ({name: s_name})
-  #
-  #
-  #    {
-  #      results: [{
-  #     name: "Spock",
-  #     species: "Vulcan",
-  #     gender: "Male",
-  #     homeworld: "Vulcan",
-  #     prelude: "Live long and prosper",
-  #     image_name: s.name + ".jpg",
-  #     image_url:"http://images2.fanpop.com/image/photos/10800000/Mr-Spock-mr-spock-10874060-1036-730.jpg"
-  #
-  #   },
-  #   {
-  #     name:"Spock Nu",
-  #     species: "Human",
-  #     gender: "Male",
-  #     homeworld: "Vulcan",
-  #     prelude: "Live long and prosper",
-  #     image_url: "http://images2.fanpop.com/image/photos/10800000/Mr-Spock-mr-spock-10874060-1036-730.jpg"
-  #   }]
-  # }.to_json
 
   s_name = params[:name]
-  s = Character.where('name LIKE ?', '%#{s_name}%').all
-
-# if s.affiliation.any?
-#   affiliation_hashes = s.affiliations.map do |u|
-#     { name: u.name }
-#   end
-# end
-
-
+  s = Character.where('name LIKE ?', '%"#{s_name}%"').all
+  # s_id = params[:id]
+  # s = Character.find s_id
+# works when you search by character id. Does not work if you search by name
+  affiliation_hashes = s.affiliations.map do |u|
+    { name: u.name }
+  end
 
 
 
-      results= {
-      name: s.name.split("\n")[0],#added because of database population mistake. Fixed mistake for future populations but did not want to rerun populate_table
+
+
+{
+      results: [{
+      name: s.name.split("\n")[0],#added because of database population mistake. Fixed mistake for future populations but did not want to rerun populate_table. Could character update instead but nah.
       species: s.species,
       gender: s.gender,
       homeworld: s.homeworld,
-      image_name: s.name + ".jpg",
+      image_name: s.name.split[0]+ s.id.to_s + ".jpg",
       image_url: s.image_url,
-      # affiliation: affiliation_hashes
-
+      prelude: s.prelude,
+      affiliation: affiliation_hashes
+    }]
     }.to_json
 
 
   end
+
 
 
   get "/affiliation/:affiliation" do
     t_name = params[:affiliation]
-    t = Affiliation.find t_name
+    t = Affiliation.where('name LIKE ?', '%"#{t_name}%"').all
+
 
     character_hashes = t.characters.map do |u|
-      { name: u.name }
+      { name: u.name.split("\n")[0], }
     end
 
-
-      results = {
+{
+      results: [{
         name: t.name,
         characters: character_hashes
-    }.to_json
+    }]
+  }.to_json
   end
 end
+
 
 App.run!
